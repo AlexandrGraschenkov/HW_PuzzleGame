7d58916ca621e3b6d1987c3bdceecc178066f532
Arthur Sagidulin <iwasraisedinacity@gmail.com>
2015-03-21 00:34:10 +0300

    Complete

---
+++ HW_SlicedImages/FolderCell.h
@@ -0,0 +1,15 @@
+//
+//  FolderCell.h
+//  HW_SlicedImages
+//
+//  Created by Артур Сагидулин on 19.02.15.
+//  Copyright (c) 2015 Артур Сагидулин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FolderCell : UITableViewCell
+
+@property (nonatomic,strong) NSDictionary *folder;
+
+@end
